a899d19c3a0fd560478ea72d1e41cc0bf194dce0
ParkerCF <faul.parker@outlook.com>
2022-11-15 19:56:36 -0500

    second commit, html updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,14 @@ var searchFormEl = document.querySelector('#search-form');
 function handleSearchFormSubmit(event) {
   event.preventDefault();
 
-  var searchInputVal = document.querySelector('#search-input').value;
-  var formatInputVal = document.querySelector('#format-input').value;
+  var searchInputVal = document.querySelector('#search-input').value
 
   if (!searchInputVal) {
     console.error('You need a search input value!');
     return;
   }
 
-  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+  var queryString = './search-results.html?q=' + searchInputVal;
 
   location.assign(queryString);
 }

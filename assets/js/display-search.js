b8e4f8de45a8925803b859361dca0226e527bf6b
ParkerCF <faul.parker@outlook.com>
2022-11-22 20:27:34 -0500

    adding js code for movie title and thumbnail

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -1,112 +1,112 @@
-var searchFormEl = document.querySelector('#search-form');
 
-function getParams() {
-  // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
-  var searchParamsArr = document.location.search.split('&');
+const options = {
+	method: 'GET',
+  s: ,
+	headers: {
+		'X-RapidAPI-Key': 'af3c4ac556msh4800601e994581cp141e52jsn02c4fb8865e3',
+		'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
+	}
+};
 
-  // Get the query and format values
-  var query = searchParamsArr[0].split('=').pop();
-  var format = searchParamsArr[1].split('=').pop();
 
-  searchApi(query, format);
-}
+userInput = document.querySelector(#search-input).value 
 
-function printResults(resultObj) {
-  console.log(resultObj);
-
-  // set up `<div>` to hold result content
-  var resultCard = document.createElement('div');
-  resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
-
-  var resultBody = document.createElement('div');
-  resultBody.classList.add('card-body');
-  resultCard.append(resultBody);
-
-  var titleEl = document.createElement('h3');
-  titleEl.textContent = resultObj.title;
-
-  var bodyContentEl = document.createElement('p');
-  bodyContentEl.innerHTML =
-    '<strong>Date:</strong> ' + resultObj.date + '<br/>';
-
-  if (resultObj.subject) {
-    bodyContentEl.innerHTML +=
-      '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
-  } else {
-    bodyContentEl.innerHTML +=
-      '<strong>Subjects:</strong> No subject for this entry.';
-  }
-
-  if (resultObj.description) {
-    bodyContentEl.innerHTML +=
-      '<strong>Description:</strong> ' + resultObj.description[0];
-  } else {
-    bodyContentEl.innerHTML +=
-      '<strong>Description:</strong>  No description for this entry.';
-  }
-
-  var linkButtonEl = document.createElement('a');
-  linkButtonEl.textContent = 'Read More';
-  linkButtonEl.setAttribute('href', resultObj.url);
-  linkButtonEl.classList.add('btn', 'btn-dark');
-
-  resultBody.append(titleEl, bodyContentEl, linkButtonEl);
-
-  resultContentEl.append(resultCard);
-}
-
-function searchApi(query, format) {
-  var locQueryUrl = 'https://www.loc.gov/search/?fo=json';
-
-  if (format) {
-    locQueryUrl = 'https://www.loc.gov/' + format + '/?fo=json';
-  }
-
-  locQueryUrl = locQueryUrl + '&q=' + query;
-
-  fetch(locQueryUrl)
-    .then(function (response) {
-      if (!response.ok) {
-        throw response.json();
-      }
-
+fetch('https://movie-database-alternative.p.rapidapi.com/?s='+ userInput + 'r=json&page=1', options)
+    .then((response) => {
+    if (response.ok) {
       return response.json();
-    })
-    .then(function (locRes) {
-      // write query to page so user knows what they are viewing
-      resultTextEl.textContent = locRes.search.query;
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => {
+    console.log(data);
+    displayMovie(data)
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
 
-      console.log(locRes);
+ function displayMovie(data) {
+    const movie = data.Search[0];
+    const movieDiv = document.getElementById("Movie");
+    const movieTitle = movie.Title;
+    const heading = document.createElement("h1");
+    heading.innerHTML = movieTitle;
+    
 
-      if (!locRes.results.length) {
-        console.log('No results found!');
-        resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
-      } else {
-        resultContentEl.textContent = '';
-        for (var i = 0; i < locRes.results.length; i++) {
-          printResults(locRes.results[i]);
-        }
-      }
-    })
-    .catch(function (error) {
-      console.error(error);
+    const movieLink = document.createElement("a");
+    movieLink.setAttribute('href', 'https://www.imdb.com/title/' + movie.imdbID) 
+    
+
+    const moviePoster = document.createElement("img");
+    moviePoster.src = movie.Poster
+
+    movieLink.appendChild(moviePoster);
+    movieDiv.appendChild(heading);  
+    movieDiv.appendChild(movieLink);
+
+  }   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function start() {
+  // Initializes the client with the API key and the Translate API.
+  gapi.client.init({
+    'apiKey': 'AIzaSyCOYLbVBlcpAeEP3EhpHdBImhg3srwnObE',
+    'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'],
+      
+  }).then(onClientInitSucess).catch(() => { console.log("failure") })
+}
+// Called automatically when YouTube API interface is loaded (see line 9).
+function onYouTubeApiLoad() {
+  gapi.client.setApiKey('AIzaSyCOYLbVBlcpAeEP3EhpHdBImhg3srwnObE');
+}
+
+// Called when the search button is clicked in the html code
+
+function onClientInitSucess() {
+  searchBtn.addEventListener('click', search)
+}
+
+var searchBtn = document.querySelector('btn')
+gapi.load('client', start)
+
+
+
+
+
+function search() {
+    var query = document.getElementById('search-input').value;
+    // Use the JavaScript client library to create a search.list() API call.
+    var request = gapi.client.youtube.search.list({
+    part: 'snippet',
+        q:query
     });
+    // Send the request to the API server, call the onSearchResponse function when the data is returned
+    request.execute(onSearchResponse);
+}
+// Triggered by this line: request.execute(onSearchResponse);
+function onSearchResponse(response) {
+    var responseString = JSON.stringify(response, '', 2);
+    document.getElementById('movie-results').innerHTML = responseString;
 }
 
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
 
-  var searchInputVal = document.querySelector('#search-input').value;
-  var formatInputVal = document.querySelector('#format-input').value;
 
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
-  }
 
-  searchApi(searchInputVal, formatInputVal);
-}
 
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-
-getParams();
